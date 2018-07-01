f05c44cadfd512c3b9065365cbcf27400eba4aa8
sixox <moh3n.dehghani@gmail.com>
2018-07-01 10:10:17 +0430

    scafolding

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,11 +28,11 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
+        format.html {redirect_to @product, notice: 'Product was successfully created.'}
+        format.json {render :show, status: :created, location: @product}
       else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+        format.html {render :new}
+        format.json {render json: @product.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -42,11 +42,11 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
-        format.json { render :show, status: :ok, location: @product }
+        format.html {redirect_to @product, notice: 'Product was successfully updated.'}
+        format.json {render :show, status: :ok, location: @product}
       else
-        format.html { render :edit }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @product.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -56,19 +56,19 @@ class ProductsController < ApplicationController
   def destroy
     @product.destroy
     respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to products_url, notice: 'Product was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def product_params
-      params.require(:product).permit(:title, :description, :image_url, :price)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def product_params
+    params.require(:product).permit(:title, :description, :image_url, :price)
+  end
 end
